3b3e3a84c6ad10d736779080863ab3fac0e4c624
Tyshna <138208931+Tyshna@users.noreply.github.com>
2025-05-09 21:19:53 +0400

    2024-25 week 3 code

---
+++ w3_q2_p1.java
@@ -0,0 +1,13 @@
+package week3;
+
+import java.util.Scanner;
+public class w3_q2_p1
+{
+	Scanner sc = new Scanner(System.in);
+	int num;
+	public void guess()
+	{
+		System.out.println("Guess the number:");
+		num = sc.nextInt();
+	}//guess
+}//player
